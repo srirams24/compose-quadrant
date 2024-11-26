f9430d17f19e2e97c785763961cd48dcee4c73bd
Sriram Sekar <124426883+srirams24@users.noreply.github.com>
2024-11-26 13:44:37 +0530

    feat: Add title and description text parameters to ComposableCard composable and update Card1 layout
    - Add title and description parameters to ComposableCard
    
    - Arrange and aligned the text compose for title and description text vertically
    
    - Set font weight to bold for the title text
    
    - Align description text to justified
    
    - Update ComposableCard usage in ComposeQuadrant with string and color resources for Card1.

--- app/src/main/java/com/example/composequadrant/MainActivity.kt
+++ app/src/main/java/com/example/composequadrant/MainActivity.kt
@@ -4,12 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composequadrant.ui.theme.ComposeQuadrantTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,18 +40,36 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ComposeQuadrant(modifier: Modifier = Modifier) {
-
+    ComposableCard(
+        title = stringResource(R.string.card1_title_text),
+        description = stringResource(R.string.card1_description_text),
+        modifier = Modifier
+            .background(color = colorResource(R.color.card1_color))
+    )
 }
 
 @Composable
-fun ComposableCard(modifier: Modifier = Modifier) {
-
+fun ComposableCard(title: String, description: String, modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+            .padding(16.dp)
+    ) {
+        Text(
+            text = title, fontWeight = FontWeight.Bold, modifier = Modifier.padding(bottom = 16.dp)
+        )
+        Text(
+            text = description,
+            textAlign = TextAlign.Justify
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun ComposeQuadrantPreview() {
     ComposeQuadrantTheme {
-        ComposableCard()
+        ComposeQuadrant()
     }
 }
